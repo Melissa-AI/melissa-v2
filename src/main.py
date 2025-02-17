be72c76698f9c22b048ea90e73d86acf4a93ed8c
Harshil <ghagrawal17@gmail.com>
2025-02-17 23:43:38 +0100

    implement tts and change to profile.json

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
 from stt import stt
+from tts import tts
+import json
+
+# Load the profile.json file
+with open("profile.json") as f:
+	profile = json.load(f)
+
+name = profile["name"]
+
+tts("Hello "+name +", I am Melissa. How can I help you?")
 
 userInput = stt()
 print(userInput)
+
+tts("You said: "+userInput)
