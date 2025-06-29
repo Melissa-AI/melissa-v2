c53c00c10f505303a0f03c5f70ee5684967c796e
Harshil <ghagrawal17@gmail.com>
2025-06-29 17:39:10 +0200

    minor updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# melissa-v2
+# Melissa: Your virtual AI assistant
+
+Melissa is a virtual asistant for MacOS, Windows, and Linux system. It uses ollama to access local LLM models, and Vosk for speech-to-text-conversion.
+
+The code is part of the book - Building a Virtual Assistant for Raspberry Pi (Edition 2), written by [Harshil Agrawal](https://harshil.dev).
+
+## Support
+
+If you run into any issues, please open up a new issue.
+
+## License
+
+[MIT License](./LICENSE)
--- src/tts.py
+++ src/tts.py
@@ -2,17 +2,18 @@ import os
 import sys
 import shlex
 
+
 def tts(message):
-	"""
-	This function takes a message as an argument and converts it to speech depending on the OS.
-	"""
+    """
+    This function takes a message as an argument and converts it to speech depending on the OS.
+    """
 
-	message = str(message)
-	escaped_message = shlex.quote(message)
+    message = str(message)
+    escaped_message = shlex.quote(message)
 
-	if sys.platform == 'darwin':
-		tts_engine = 'say'
-		return os.system(f'{tts_engine} {escaped_message}')
-	elif sys.platform == 'linux2' or sys.platform == 'linux':
-		tts_engine = 'espeak'
-		return os.system(f'{tts_engine} {escaped_message}')
+    if sys.platform == 'darwin':
+        tts_engine = 'say'
+        return os.system(f'{tts_engine} {escaped_message}')
+    elif sys.platform == 'linux2' or sys.platform == 'linux':
+        tts_engine = 'espeak'
+        return os.system(f'{tts_engine} {escaped_message}')
